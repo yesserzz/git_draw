a7a5a296e907276de5b716eaa83b3eefa8d54dff
yesserz <dr.giroks2017@ya.ru>
2024-08-08 23:37:19 +0300

    aa

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import os
+import subprocess
+from datetime import datetime, timedelta
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Настройка
+repository_path = 'my-github-drawing-repo'  # Путь к репозиторию
+start_date = datetime(2022, 1, 1)  # Дата начала, от которой будем отсчитывать
+width, height = 53, 7  # Размер сетки активности (GitHub показывает 53 недели по 7 дней)
 
+# Текст для отображения на графике активности
+drawing = [
+    "  ###   ###   #   #   ###   ##### ",
+    " #   # #   #  ##  #  #   #  #     ",
+    "##### #####  # # #  #   #  ####  ",
+    "#   # #   #  #  ##  #   #  #     ",
+    "#   # #   #  #   #   ###   ##### "
+]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Функция, которая создает коммит с заданной датой
+def make_commit(date):
+    with open(os.path.join(repository_path, 'README.md'), 'a') as file:
+        file.write(f'Commit on {date.isoformat()}\n')
+    subprocess.run(['git', 'add', '.'], cwd=repository_path)
+    subprocess.run(['git', 'commit', '--date', date.isoformat(), '-m', f'Commit on {date}'], cwd=repository_path)
 
+# Создание пустого репозитория
+os.makedirs(repository_path, exist_ok=True)
+subprocess.run(['git', 'init'], cwd=repository_path)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Создание коммитов для каждого символа в тексте
+for y in range(len(drawing)):
+    for x in range(len(drawing[y])):
+        if drawing[y][x] == '#':
+            commit_date = start_date + timedelta(weeks=x, days=y)
+            make_commit(commit_date)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Завершение
+print("Коммиты завершены. Теперь отправьте изменения в ваш репозиторий на GitHub.")
